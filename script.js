e1167acdde179330c8d2233fb91d28485dd765bb
Matheus Carvalho <73304785+imath5@users.noreply.github.com>
2023-01-19 12:14:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var valorDolar = prompt ("Qual o valor em dólar você quer converter ?")
+var valorDolar = parseFloat(valorDolar)
+var valorReal = valorDolar *5.22
+var valorRealDec = valorReal.toFixed(2)
+
+  alert(+ valorDolar + "U$ são: " + valorRealDec + "R$")
+
+/*Revisão 
+variáveis ex: var int/float/string
+alert - prompt - parseInt/parseFloat
+operações - +somar/*multiplicar
+
+
+*/
